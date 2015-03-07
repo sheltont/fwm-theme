7bafbdace90a7b9e12c505f0f129838a29905916
Shao Liang <liang.shao@binqsoft.com>
2015-03-07 14:10:45 +0800

    IMP: frontend

---
+++ skin/fwm/default/js/ext.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Leon on 15/3/6.
+ */
+(function (win) {
+    win.ext = {};
+    var $ = jQuery;
+})(this);
---
+++ skin/fwm/default/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Leon on 15/3/6.
+ */
+
+(function (win) {
+    seajs.config({
+        base: "/magentoEE14/skin/frontend/fwm/default"
+    });
+    seajs.use("js/ext");
+})(window);
